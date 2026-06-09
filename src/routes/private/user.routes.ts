606ecccf7eb6c1fefb7a22f562a0ec9bf8704d66
Carlos <caique.h@hotmail.com>
2026-06-09 19:23:41 -0300

    refactor(routes): remove unused user route handlers and JWT middleware

--- src/routes/private/user.routes.ts
+++ src/routes/private/user.routes.ts
@@ -1,17 +1,5 @@
 import { Router } from 'express';
 
-import { middlewareValidateJWT } from '../../middlewares/jwt.middleware.js';
-
 const userRoutes = Router();
-
-userRoutes.use(middlewareValidateJWT);
-
-userRoutes.get('/', (req, res) => {
-  res.json({ message: 'This is the user route' });
-});
-
-userRoutes.post('/', (req, res) => {
-  res.json({ message: 'This is the user route' });
-});
 
 export default userRoutes;
