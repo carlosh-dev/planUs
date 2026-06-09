a99136d02ad72d2f4740e9eb8daf9d6c484c3a03
Carlos <caique.h@hotmail.com>
2026-06-09 19:19:59 -0300

    feat(tests): enhance orchestrator with user login functionality and clearDatabase order

--- src/tests/orchestrator.ts
+++ src/tests/orchestrator.ts
@@ -1,12 +1,38 @@
 import { prisma } from '../infra/database/prisma.js';
 
 async function clearDatabase() {
-  await prisma.user.deleteMany();
+  await prisma.transactionTag.deleteMany();
+  await prisma.transaction.deleteMany();
+  await prisma.plannedTransactionTag.deleteMany();
+  await prisma.plannedTransaction.deleteMany();
   await prisma.family.deleteMany();
+  await prisma.user.deleteMany();
+}
+
+async function getLoggedUser() {
+  const response = await fetch(`${process.env.BASE_URL}/api/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: 'user@email.com',
+      password: 'password123',
+    }),
+  });
+
+  const body = (await response.json()) as {
+    id: string;
+    name: string;
+    token: string;
+  };
+
+  return body;
 }
 
 const orchestrator = {
   clearDatabase,
+  getToken: getLoggedUser,
 };
 
 export default orchestrator;
